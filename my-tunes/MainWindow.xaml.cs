a602ec97c5f2d5ee0d78ccc71508a11adf84fb8c
Gus McCoy <51655110+gusmccoy@users.noreply.github.com>
2020-10-26 11:18:42 -0500

    Added my MusicLibrary as the base project

--- my-tunes/MainWindow.xaml.cs
+++ my-tunes/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,46 @@ namespace my_tunes
     /// </summary>
     public partial class MainWindow : Window
     {
+        private DataSet musicDataSet;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void saveBtn_Click(object sender, RoutedEventArgs e)
+        {
+            musicDataSet = new DataSet();
+            musicDataSet.ReadXmlSchema("music.xsd");
+            musicDataSet.ReadXml("music.xml");
+
+            DataTable table = musicDataSet.Tables["song"];
+            DataRow row = table.NewRow();
+            row["title"] = this.titleTxtBox.Text;
+            row["artist"] = this.artistTxtBox.Text;
+            row["album"] = this.albumTxtBox.Text;
+            row["genre"] = this.genreTxtBox.Text;
+            row["length"] = this.lengthTxtBox.Text;
+            row["filename"] = this.filenameTxtBox.Text;
+            table.Rows.Add(row);
+
+            musicDataSet.WriteXml("music.xml");
+
+        }
+
+        private void deleteBtn_Click(object sender, RoutedEventArgs e)
+        {
+            DataTable table = musicDataSet.Tables["song"];
+            DataRow row = table.Rows.Find(this.idTxtBox.Text);
+            if (row != null)
+                table.Rows.Remove(row);
+
+            musicDataSet.WriteXml("music.xml");
         }
     }
 }
